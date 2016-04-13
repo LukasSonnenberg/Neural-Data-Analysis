f820c4d026764ddb4716e4cd7f27b15d315cbfbf
PaulaHil <paula.hilsendegen@student.uni-tuebingen.de>
2016-04-13 16:24:17 +0200

    Added files via upload

--- extractWaveforms.m
+++ extractWaveforms.m
@@ -4,4 +4,27 @@ function w = extractWaveforms(x, s)
 %   samples) from the filtered signal x using a fixed window around the
 %   times of the spikes. The return value w is a 3d array of size
 %   length(window) x #spikes x #channels.
+%% define interval around local minimum build average
+time_window = 0.001;
+% window_size determines amount of data points
+samplingRate = 30000;
+window_size = samplingRate*time_window;
+% preallocation of w
+w = zeros(window_size,length(s), length(x(1,:)));
+% rel_pos_min determines the position of the spike within the window
+rel_pos_min = 0.3;
+low_limit = round(window_size*rel_pos_min);
+up_limit = window_size-low_limit;
+
+for i = 1: length(x(1,:))
+  for j = 1: length(s)
+      w(:,j,i) =(x((s(j)+1- low_limit):(s(j)+up_limit),i));
+  end
+end
+
+
+
+
+
+
 
