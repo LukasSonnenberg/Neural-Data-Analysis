647aa286e78c5d089171307daedac6f50be339ea
Niguro <Lukas.Sonnenberg@web.de>
2016-04-13 12:56:59 +0200

    Added files via upload

--- detectSpikes.m
+++ detectSpikes.m
@@ -6,11 +6,39 @@ function [s, t] = detectSpikes(x, Fs)
 %   sample is 0 ms.
 
 %get threshold
-N = 1;
+N = 10;
 sigma = median(abs(x)/0.6745);
 thresh = N*sigma;
 
-%get all points below threshold
-below_thresh = x<-thresh;
-
 %get start and end of passover of threshold
+passover = diff(bsxfun(@lt,x,-thresh));
+pass_start=cell(4,1);
+pass_end = cell(4,1);
+for i = 1:4
+    pass_start{i} = find(passover(:,i) == 1);
+    pass_end{i} = find(passover(:,i) == -1);
+    if i == 1 %if array starts with spike delete this one
+        if pass_start{i}(1)>pass_end{i}(1)
+            pass_end{i} = pass_end{i}(2:end);
+        end
+    end
+end
+
+%get time points if minima of passovers
+minima = cell(4,1);
+for i = 1:4
+    minima{i} = zeros(length(pass_start{i}),1);
+    for j = 1:length(pass_start{i})
+        [val,idx] = min(x(pass_start{i}(j):pass_end{i}(j),i));
+        minima{i}(j) = pass_start{i}(j)-1+idx;
+    end
+end
+
+%compare the four channels and assume that almost near spikes are the same
+%spike
+spikes = sort([minima{1}; minima{2}; minima{3}; minima{4}]);
+near_spikes = [0; diff(spikes)<3]; %the minima of spikes are allowed to differ by 2 timesteps to be counted as one spike
+size(near_spikes)
+s = spikes(near_spikes==0);
+t=(s-1)/Fs;
+
