381983a755788ef47c4ac1cfbbba09b601909cf9
Niguro <Lukas.Sonnenberg@web.de>
2016-04-20 18:40:41 +0200

    Added files via upload

--- Exercise2/sortSpikes.m
+++ Exercise2/sortSpikes.m
@@ -16,44 +16,89 @@ function [mu, Sigma, priors, df, assignments] = sortSpikes(b)
 D = size(b,2);
 K = 3; % erfordert noch Arbeit
 
+trials = 10;
+runs = 30;
+
 mu = zeros(K,D);
 Sigma = zeros(D,D,K);
+loglike = zeros(trials,runs);
+
 for i=1:D
-    Sigma(i,i,:) = 1;
+    Sigma(i,i,:) = 10;
 end
 assignments = zeros(1,K);
 df = inf;   % you don't need to use this variable unless you want to 
             % implement a mixture of t-distributions
-
-%% Set starting Points 
-%of mu (random) in range of each dimension
-for i = 1:D
-    mu(:,i) = min(b(:,i)) + rand(K,1)*(max(b(:,i))-min(b(:,i)));
-end
-%of the prior (every one the same), is this the right variable?
+%neurons = zeros(size(b,1),K);     
 priors = ones(1,K)/K;
-
-%% initial log likelihood
-%has yet to be done (maybe)
+min_loglike = 0;
 
 %% EM
-for i = 1:50 % length should be optimized and not like this
-    %E-Step
-    gamma = zeros(size(b,1),K,D);
-    for l = 1:size(b,1)
-        for j = 1:K
-            gamma(l,j,:) = priors(j)*mvnpdf(b(l,:),mu(j,:), Sigma(:,:,j));
-        end
-        gamma(l,:,:) = gamma(l,:,:)/sum(gamma(l,:,:),2);
+break_loop = 0;
+for t = 1:trials
+    for i = 1:D
+        mu(:,i) = min(b(:,i)) + rand(K,1)*(max(b(:,i))-min(b(:,i)));
     end
-    
-    %M-Step
-    N = sum(gamma,1);
-    for j = 1:K
-        mu(j,:) = 1/N(j)*sum(gamma(:,j,:).*b); %might be wrong
-        Sigma(:,:,j) = 1/N(j)*sum(gamma(:,j,:).*((b-mu(j,:))*(b-mu(j,:))'));
-        priors(j) = N(j)/size(b,1);
+    for i = 1:runs % length should be optimized and not like this
+        new_mu = mu*0;
+        new_Sigma = Sigma*0;
+        new_priors = priors*0;
+
+        %E-Step
+        gamma = zeros(size(b,1),K);
+        gamma2 = zeros(size(b,1),K);
+        for l = 1:size(b,1)
+            for j = 1:K     
+                gamma(l,j) = priors(j)*mvnpdf(b(l,:),mu(j,:), Sigma(:,:,j));
+            end   
+            gamma2(l,:) = gamma(l,:) == max(gamma(l,:));
+        end
+
+        gamma = bsxfun(@rdivide, gamma, sum(gamma,2));    
+
+        %M-Step
+        N = sum(gamma,1);
+        for j = 1:K
+            new_mu(j,:) = 1/N(j)*sum(bsxfun(@times,gamma(:,j),b));
+            for l = 1:size(b,1)
+                b_mu = b(l,:) - new_mu(j,:);
+                new_Sigma(:,:,j) = new_Sigma(:,:,j) + 1/N(j)*(b_mu'*b_mu)*gamma(l,j);
+            end
+            new_priors(j) = N(j)/size(b,1);
+        end
+        
+        for j = 1:K
+            if sum(diag(Sigma(:,:,j)) < 0.1) > 0
+                break_loop = 1;
+            end
+        end 
+        if break_loop == 1
+            break
+        end
+        
+        for l = 1:size(b,1)
+            for j = 1:K
+                loglike(t,i) = loglike(t,i) + log(sum(new_priors.*mvnpdf(b(l,:),mu(j,:),Sigma(:,:,j))));
+            end
+        end
+        if loglike(t,i) == -inf
+            loglike(t,i:end) = nan;
+            break
+        end
+        
+        mu = new_mu;
+        Sigma = new_Sigma;
+        priors = new_priors;
+    end
+    if loglike(t,end) < min_loglike
+        min_mu = mu;
+        min_Sigma = Sigma;
+        min_priors = priors;
+        min_gamma = gamma2;
     end
 end
-    
 
+mu = min_mu;
+Sigma = min_Sigma;
+priors = min_priors;
+assignments = sum(bsxfun(@times,min_gamma,1:K),2);
