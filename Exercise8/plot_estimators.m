a4992a285527f2c476cf3f8d7440325b2c7817ab
Niguro <Lukas.Sonnenberg@web.de>
2016-06-16 14:32:25 +0200

    Add files via upload

--- Exercise8/plot_estimators.m
+++ Exercise8/plot_estimators.m
@@ -1,4 +1,4 @@
-function plot_estimators(H,H_mle,H_mm,H_cae,H_jk,sampleSz,s)
+function plot_estimators(H,H_mle,H_mm,H_cae,H_jk,BUB_est,sampleSz,s)
 % function for plotting the different estimators
 %   H       scalar with true entropy
 %   H_mle   matrix nSampleSz x nRuns of entropy estimates by mle estimator
@@ -10,13 +10,15 @@ lw = 2;
 ms = 2;
 
 figure
-ma = max(reshape([H_mle,H_mm,H_cae,H_jk],[],1));
-mi = min(reshape([H_mle,H_mm,H_cae,H_jk],[],1));
+ma = max(reshape([H_mle,H_mm,H_cae,H_jk,BUB_est],[],1));
+mi = min(reshape([H_mle,H_mm,H_cae,H_jk,BUB_est],[],1));
 line([sampleSz(1)*.7 sampleSz(end)*1.3],[H H],'color','k'), hold on
 plot(sampleSz,mean(H_mle,2),'rs-','linewidth',lw,'markersize',ms), hold on
 plot(sampleSz,mean(H_mm,2),'bs-','linewidth',lw,'markersize',ms), hold on
 errorbar(sampleSz,mean(H_jk,2),std(H_jk,[],2),'cs-','linewidth',lw,'markersize',ms), hold on
 errorbar(sampleSz,mean(H_cae,2),std(H_cae,[],2),'gs-','linewidth',lw,'markersize',ms), hold on
+errorbar(sampleSz,mean(BUB_est,2),std(BUB_est,[],2),'ms-','linewidth',lw,'markersize',ms), hold on
+
 set(gca,'xscale','log')
 title(s)
 xlabel('Number of Samples (N)')
@@ -24,4 +26,4 @@ ylabel('Entropy (bits)')
 xlim([sampleSz(1)*.7 sampleSz(end)*1.3])
 ylim([0.95*mi 1.05*ma])
 
-legend('H','MLE','MLE+MM','JK','CAE','location','southeast'); legend('boxoff')
+legend('H','MLE','MLE+MM','JK','CAE','BUB','location','southeast'); legend('boxoff')
